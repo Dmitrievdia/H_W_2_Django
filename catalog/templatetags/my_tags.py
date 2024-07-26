0daa80597a825082e0b719fbffc6075218246a26
dia <zond86@mail.ru>
2024-07-26 22:59:50 +0300

    feat: изменения

--- catalog/templatetags/my_tags.py
+++ catalog/templatetags/my_tags.py
@@ -1,9 +1,9 @@
 from django import template
 
-register = template.Library
+register = template.Library()
 
 
-@register.filter()
+@register.filter
 def media_filter(path):
     if path:
         return f"/media/{path}"
