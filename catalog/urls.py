628d2f2034e484312cd978fc25b812b5c6d265ed
dia <zond86@mail.ru>
2024-09-06 12:10:43 +0300

    feat: load CBV controller

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,29 +1,11 @@
-# from django.urls import path, include
-# from catalog.apps import CatalogConfig
-# from catalog.views import index
-# app_name = CatalogConfig.name
-#
-# urlpatterns = [
-#     path('', include('catalog.urls', namespace='catalog')),
-# ]
 from django.urls import path
+
 from catalog.apps import CatalogConfig
-from catalog.views import index, products_detail
+from catalog.views import ProductListView, ProductDetailView
 
 app_name = CatalogConfig.name
 
-# urlpatterns = [
-#     path('', home, name='home'),
-#     path('contacts/', contacts, name='contacts')
-# ]
-
-
 urlpatterns = [
-    path('', index, name='product_list'),
-    path('catalog/<int:pk>/', products_detail, name='products_detail')
+    path('', ProductListView.as_view(), name='product_list'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='products_detail')
 ]
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('', include('products.urls', namespace='products'))
-# ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,43 +1,23 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+
 from catalog.models import Product, Category
 
 
-# def home(request):
-#     return render(request, 'home.html')
-#
-#
-# def contacts(request):
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         phone = request.POST.get('phone')
-#         message = request.POST.get('message')
-#         print(f'{name} ({phone}): {message}')
-#     return render(request, 'contacts.html')
-
-# def products(request):
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         phone = request.POST.get('phone')
-#         message = request.POST.get('message')
-#         print(f'{name} ({phone}): {message}')
-#     return render(request, catalog/temlates/includes/'products.html')
-# def index(request):
-#     return render(request, 'base.html')
-
+class ProductListView(ListView):
+    model = Product
+    # catalog/product_list.html
 
 # def index(request):
-#     products_list = Product.objects.all()
-#     context = {"odject_list": products_list}
-#     return render(request, 'base.html', context)
-
-def index(request):
-    products = Product.objects.all()
-    context = {'products': products}
-    return render(request, 'product_list.html', context)
+#     products = Product.objects.all()
+#     context = {'products': products}
+#     return render(request, 'product_list.html', context)
 
 
-# from django.shortcuts import render
-def products_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {'product': product}
-    return render(request, 'products_detail.html', context)
+class ProductDetailView(DetailView):
+    model = Product
+
+# def products_detail(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+    # context = {'product': product}
+    # return render(request, 'products_detail.html', context)
