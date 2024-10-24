3e55ca5bb8ed7e599db31e0f84e6a3b09ba85e93
dia <zond86@mail.ru>
2024-10-24 16:24:08 +0300

    feat: Перевели имеющиеся контроллеры с FBV на CBV.

--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from catalog.models import Product, Category
 
 
 class ProductListView(ListView):
     model = Product
-    # catalog/product_list.html
+    template_name = 'catalog/product_list.html'
 
 # def index(request):
 #     products = Product.objects.all()
@@ -16,8 +17,25 @@ class ProductListView(ListView):
 
 class ProductDetailView(DetailView):
     model = Product
-
+    template_name = 'catalog/products_detail.html'
 # def products_detail(request, pk):
 #     product = get_object_or_404(Product, pk=pk)
     # context = {'product': product}
     # return render(request, 'products_detail.html', context)
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ("title", "description", "avatar", "category", "price")
+    success_url = reverse_lazy('catalog:product_list')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ("title", "description", "avatar", "category", "price")
+    success_url = reverse_lazy('catalog:product_list')
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('catalog:product_list')
