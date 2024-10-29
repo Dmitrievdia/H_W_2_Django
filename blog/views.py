73e31731e9d1759d6465b92d1e20f1b837a95163
dia <zond86@mail.ru>
2024-10-29 15:17:47 +0300

    feat: add see counter

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.utils.text import slugify
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 
 from blog.models import Article
@@ -10,11 +11,28 @@ class ArticleCreateView(CreateView):
     fields = ('title', 'content', 'preview', 'published', 'number_views',)
     success_url = reverse_lazy('blog:blog_list')
 
+    def form_valid(self, form):
+        if form.is_valid():
+            new_art = form.save()
+            new_art.slug = slugify(new_art.title)
+            new_art.save()
+        return super().form_valid(form)
+
 
 class ArticleUpdateView(UpdateView):
     model = Article
     fields = ('title', 'content', 'preview', 'published', 'number_views',)
-    success_url = reverse_lazy('blog:blog_list')
+    # success_url = reverse_lazy('blog:blog_list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_art = form.save()
+            new_art.slug = slugify(new_art.title)
+            new_art.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blog:blog_detail', args=[self.kwargs.get('pk')])
 
 
 class ArticleListView(ListView):
@@ -22,10 +40,21 @@ class ArticleListView(ListView):
     template_name = 'blog/article_list.html'
     context_object_name = 'posts'
 
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(published=True)
+        return queryset
+
 
 class ArticleDetailView(DetailView):
     model = Article
     # template_name = reverse_lazy('blog:article_detail.html')
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.number_views += 1
+        self.object.save()
+        return self.object
 
 
 class ArticleDeleteView(DeleteView):
